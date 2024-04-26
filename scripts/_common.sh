dbfd8a66aff7142120b10ee8c5e9faa9a4873b07
Josué Tille <josue@tille.ch>
2024-04-27 01:00:20 +0200

    Fix shell check

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -24,7 +24,7 @@ else
     install_on_root=false
     path2=${path/#\//}/ # path=/xwiki -> xwiki/
     path3=${path/#\//} # path=/xwiki -> xwiki
-    web_inf_path="$install_dir"/webapps$path/WEB-INF
+    web_inf_path="$install_dir/webapps$path/WEB-INF"
 fi
 
 #=================================================
@@ -34,18 +34,18 @@ fi
 enable_super_admin() {
     super_admin_pwd=$(ynh_string_random)
     super_admin_config="xwiki.superadminpassword=$super_admin_pwd"
-    ynh_add_config --template=xwiki.cfg --destination=/etc/$app/xwiki_conf.cfg
-    ln -f /etc/$app/xwiki_conf.cfg "$web_inf_path"/xwiki.cfg
-    chmod 400 /etc/$app/xwiki_conf.cfg
-    chown "$app:$app" /etc/$app/xwiki_conf.cfg
+    ynh_add_config --template=xwiki.cfg --destination=/etc/"$app"/xwiki_conf.cfg
+    ln -f /etc/"$app"/xwiki_conf.cfg "$web_inf_path"/xwiki.cfg
+    chmod 400 /etc/"$app"/xwiki_conf.cfg
+    chown "$app:$app" /etc/"$app"/xwiki_conf.cfg
 }
 
 disable_super_admin() {
     super_admin_config='#'
-    ynh_add_config --template=xwiki.cfg --destination=/etc/$app/xwiki_conf.cfg
-    ln -f /etc/$app/xwiki_conf.cfg "$web_inf_path"/xwiki.cfg
-    chmod 400 /etc/$app/xwiki_conf.cfg
-    chown "$app:$app" /etc/$app/xwiki_conf.cfg
+    ynh_add_config --template=xwiki.cfg --destination=/etc/"$app"/xwiki_conf.cfg
+    ln -f /etc/"$app"/xwiki_conf.cfg "$web_inf_path"/xwiki.cfg
+    chmod 400 /etc/"$app"/xwiki_conf.cfg
+    chown "$app:$app" /etc/"$app"/xwiki_conf.cfg
 }
 
 install_exension() {
@@ -66,12 +66,12 @@ install_exension() {
     local status_raw
     local state_request
 
-    chmod 700 $temp_dir
-    chown root:root $temp_dir
+    chmod 700 "$temp_dir"
+    chown root:root "$temp_dir"
 
-    ynh_add_config --template=install_extensions.xml --destination=$temp_dir/install_extensions.xml
+    ynh_add_config --template=install_extensions.xml --destination="$temp_dir"/install_extensions.xml
     status_raw=$($curl -i --user "superadmin:$super_admin_pwd" -X PUT -H 'Content-Type: text/xml' "http://127.0.0.1:$port/${path2}rest/jobs?jobType=install&async=true" --upload-file $temp_dir/install_extensions.xml)
-    state_request=$(echo $status_raw | $xq -x '//jobStatus/ns2:state')
+    state_request=$(echo "$status_raw" | $xq -x '//jobStatus/ns2:state')
 
     while true; do
         sleep 5
@@ -152,14 +152,14 @@ install_source() {
     elif [ "$path" == /root ]; then
         ynh_die --message='Path "/root" not supported'
     elif [ "$path" != /xwiki ]; then
-        mv "$install_dir"/webapps/xwiki "$install_dir"/webapps$path
+        mv "$install_dir"/webapps/xwiki "$install_dir/webapps$path"
     fi
 }
 
 add_config() {
-    ynh_add_config --template=hibernate.cfg.xml --destination=/etc/$app/hibernate.cfg.xml
-    ynh_add_config --template=xwiki.cfg --destination=/etc/$app/xwiki_conf.cfg
-    ynh_add_config --template=xwiki.properties --destination=/etc/$app/xwiki_conf.properties
+    ynh_add_config --template=hibernate.cfg.xml --destination=/etc/"$app"/hibernate.cfg.xml
+    ynh_add_config --template=xwiki.cfg --destination=/etc/"$app"/xwiki_conf.cfg
+    ynh_add_config --template=xwiki.properties --destination=/etc/"$app"/xwiki_conf.properties
 
     # Note that using /etc/xwiki/xwiki.cfg or /etc/xwiki/xwiki.properties is hard coded on the application
     # And using this break multi instance feature so we must use an other path
@@ -172,16 +172,16 @@ set_permissions() {
     chmod -R u+rwX,o-rwx "$install_dir"
     chown -R "$app:$app" "$install_dir"
 
-    chmod -R u=rwX,g=rX,o= /etc/$app
-    chown -R "$app:$app" /etc/$app
+    chmod -R u=rwX,g=rX,o= /etc/"$app"
+    chown -R "$app:$app" /etc/"$app"
 
-    chown "$app:$app" -R /var/log/$app
-    chmod u=rwX,g=rX,o= -R /var/log/$app
+    chown "$app:$app" -R /var/log/"$app"
+    chmod u=rwX,g=rX,o= -R /var/log/"$app"
 
-    find $data_dir \(   \! -perm -o= \
-                    -o \! -user $app \
-                    -o \! -group $app \) \
-                -exec chown $app:$app {} \; \
+    find "$data_dir" \(   \! -perm -o= \
+                    -o \! -user "$app" \
+                    -o \! -group "$app" \) \
+                -exec chown "$app:$app" {} \; \
                 -exec chmod u=rwX,g=rX,o= {} \;
 }
 
